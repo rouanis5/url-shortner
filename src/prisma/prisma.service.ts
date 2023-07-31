4c4f6e16a7bc406731a9dea74e6898a3bd93a898
rouanis5 <ouanis42@gmail.com>
2023-07-31 16:29:14 +0100

    prisma disconnect

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   // The onModuleInit is optional — if you leave it out,
   // Prisma will connect lazily on its first call to the database.
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
