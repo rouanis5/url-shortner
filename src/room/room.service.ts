84c682cc28474ebe533f0eebc7376ada446bc7f5
rouanis5 <ouanis42@gmail.com>
2023-08-09 15:21:25 +0100

    add room services

--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,8 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface ICreateRoom {
+  userId: string;
+  name: string;
+  private?: boolean;
+}
 
 @Injectable()
 export class RoomService {
-  welcome() {
-    return { message: 'welcome to Rooms' };
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getRoom(id: string) {
+    const room = await this.prismaService.room.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!room) {
+      throw new NotFoundException();
+    }
+
+    return room;
   }
+
+  async createRoom(params: ICreateRoom) {
+    return await this.prismaService.room.create({
+      data: {
+        name: params.name,
+        private: params.private,
+        members: {
+          create: {
+            isAdmin: true,
+            isActivated: true,
+            userId: params.userId,
+          },
+        },
+      },
+    });
+  }
+
+  async getSubscribedRooms(userId: string) {
+    // also include the not activated and banned rooms
+    return await this.prismaService.room.findMany({
+      where: {
+        members: {
+          some: {
+            userId,
+          },
+        },
+      },
+    });
+  }
+
+  // TODO: maybe someday when I'll integrate Redis
+  // async inviteToRoom() {}
 }
